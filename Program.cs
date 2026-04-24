abacf0265848e97f13e380b8fc12f7d4bd47d460
PBL075 <pavelgorea75@gmail.com>
2026-04-24 15:41:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Perekupshik_Ion
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine ("Cu cat ai cumparat telefonul?");
+            int pretCumparare = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("Dar cu cat lai vandut? ");
+            int pretVanzare = Convert.ToInt32 (Console.ReadLine());
+
+            int Profit = pretVanzare - pretCumparare ;
+
+            Console.WriteLine(" Profitul tau este " + Profit + " Euro");
+        }
+    }
+}
